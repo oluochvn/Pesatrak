6ae275bc83dd68a992172dd57e501b0d4f025730
vnn@dell <red.oluoch@gmail.com>
2026-05-01 16:16:25 +0300

    setup vite

--- pesafront/src/App.tsx
+++ pesafront/src/App.tsx
@@ -2,10 +2,18 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="min-h-[100vh] bg-white">
+      <nav className="flex h-12 /60 backdrop-blur-sm  justify-between p-3 bg-[#FDFEFF]">
+        <div className="">
+            <h2>Pesatrak</h2>
+        </div>
+        <ul className="flex gap-4">
+          <li>Componets</li>
+          <li>Templates</li>
+          <li>Contact</li>
+          <li>Sign in</li>
+        </ul>
+      </nav>
     </div>
   )
 }
